f71d21f4aa4777395fc177989b5b7fc465afbf2d
Mohmmadasif Shaikh <166924901+mohmmadasif@users.noreply.github.com>
2024-04-14 19:43:08 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,11 @@
+#This is the code to make my own Music Bandname Generator by just concatenating two strings.
+
+#First of all, the Greetings of course.
+print("Welcom to the Music Bandname Generator")
+
+#inputs
+name = input("What's the city that you grew up in ?")
+pet = input("What is the name of your pet? \n")
+
+#your bandname
+print=("From our calculations, your brand name should be"+name+""+pet)
